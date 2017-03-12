45d3437a574778cdfeb7a2be58244b50214a502e
samr28 <samroq@yahoo.com>
2017-03-12 09:42:10 -0500

    Fixed some static things

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,15 +9,7 @@ import org.opencv.core.MatOfPoint;
 import org.opencv.imgproc.Imgproc;
 
 public class Main {
-  public static void main(String[] args) {	
-	final int LOGITECH_RES_WIDTH = 480;					//720
-	final int LOGITECH_RES_HEIGHT = 640;				//1280
-	final int LOGITECH_HORIZ_ANGLE = 45;				//Angle of camera relative to ground
-	final int UPPER_TAPE_WIDTH = 4;
-	final double LOGITECH_FOCAL_LENGTH = 554.256;		//Slide 42 [640/(2*tan(60/2))]
-	final double LOGITECH_FOV = 46.826					//Slide 42 [2atan((.5*480)/554.256)]
-	final int TOWER_HEIGHT = 88;
-	  
+  public static void main(String[] args) {
     NetworkTable.setClientMode();
     NetworkTable.setTeam(1306);
     NetworkTable.initialize();
@@ -36,6 +28,7 @@ public class Main {
     UsbCamera camera = setUsbCamera(0, inputStream);
     // Set the resolution for our camera, since this is over USB
     camera.setResolution(1280,720);
+    
 
     // This creates a CvSink for us to use. This grabs images from our selected camera, 
     // and will allow us to use those images in opencv
@@ -48,20 +41,22 @@ public class Main {
     MjpegServer cvStream = new MjpegServer("CV Image Stream", 1186);
     cvStream.setSource(imageSource);
 
-    // Create objects here to avoid allocation issues
+    // All Mats and Lists should be stored outside the loop to avoid allocations
+    // as they are expensive to create
     Mat inputImage = new Mat();
-	Pipeline pipeline = new Pipeline();
-	ArrayList<Rect> bounding_box;
-	double yaw, dist, angle;
+    Mat hsv = new Mat();
+	
+    boolean hasBBOX = false;
 	
     while (true) {
 	long frameTime = imageSink.grabFrame(inputImage);
         if (frameTime == 0) continue;
-		
-	//image_process=inputImage.t();
-	//final_contours=processImage();
-	bounding_box=getBoundingBox(processImage());
-	
+	VisionData data=new VisionData(inputImage.t());
+	ArrayList<MatOfPoint> final_contours;
+	ArrayList<Rect> bounding_box;
+	double yaw, dist, angle;
+	final_contours=data.processImage();
+	bounding_box=data.getBoundingBox(final_contours);
 	// Sorting (We want the top tape, not the bottom one
 	if (bounding_box.size() > 0) {
 		java.util.Collections.sort(bounding_box, new Comparator<Rect>() {
@@ -71,20 +66,16 @@ public class Main {
 		    	return  Double.compare(bbox1.y,bbox2.y);
 			}
 	    	});
-		//yaw=getYaw(bounding_box.get(0));
-		//dist=getDistance(bounding_box.get(0));
-		//angle=getAngle(getDistance(bounding_box.get(0)));
-		
-		table.putNumber("yaw", getYaw(bounding_box.get(0)));
-		table.putNumber("dist", getDistance(bounding_box.get(0)));
-		table.putNumber("angle", getAngle(getDistance(bounding_box.get(0))));
-		
+		yaw=data.getYaw1(bounding_box.get(0));
+		dist=data.getDistance(bounding_box.get(0));
+		angle=data.getAngle(dist);
+		table.putNumber("yaw", yaw);
+		table.putNumber("dist", dist);
+		table.putNumber("angle", angle);
 		//table.putNumber("bboxx", bounding_box.get(0).x);
 		//table.putNumber("bboxy", bounding_box.get(1).y);
-		
 		table.putBoolean("seeTarget", true);
-		System.out.println("YAW:" + getYaw(bounding_box.get(0)));
-		
+		System.out.println("YAW:" + yaw);
 	} else {
 		table.putBoolean("seeTarget", false);
 		System.out.println("No bbox------------------------------------------------------------No bbox");
@@ -104,16 +95,25 @@ public class Main {
     server.setSource(camera);
     return camera;
   }
+	int LOGITECH_RES_WIDTH = 720;
+	int LOGITECH_HORIZ_ANGLE = 45;
+	int UPPER_TAPE_WIDTH = 4;
+	double LOGITECH_FOCAL_LENGTH = 0.15748;
+	int TOWER_HEIGHT = 88;
 
-	//ArrayList<MatOfPoint> final_contours; // Contours that GRIP gives at the end
+	Pipeline pipeline; // This goes to the GRIP pipeline that does all the work
+	Mat image_process; // Input image
+
 	public ArrayList<MatOfPoint> processImage() {
-		pipeline.process(inputImage.t());
-		//final_contours = pipeline.filterContoursOutput(); // Get GRIP output
-		return pipeline.filterContoursOutput();
+		ArrayList<MatOfPoint> final_contours; // Contours that GRIP gives at the end
+		pipeline.process(image_process);
+		final_contours = pipeline.filterContoursOutput(); // Get GRIP output
+		return final_contours;
 	}
 
-	ArrayList<Rect> bbox= new ArrayList<Rect>();
 	public ArrayList<Rect> getBoundingBox(ArrayList<MatOfPoint> contours) {
+		ArrayList<Rect> bbox= new ArrayList<Rect>(); //Bbox stuff
+
 		for (int i = 0; i < contours.size(); i++) {
 			bbox.add(Imgproc.boundingRect(contours.get(i)));
 		}
@@ -121,15 +121,26 @@ public class Main {
 	}
 
 	public double getYaw(Rect upper_tape) {
-		System.out.println("Upper tape X:" + upper_tape.x + "Upper tape Y:" + upper_tape.y + "imgCenter:" + (LOGITECH_RES_WIDTH/2));
-		return Math.toDegrees(Math.atan((upper_tape.x - (LOGITECH_RES_WIDTH/2)) / LOGITECH_FOC_LENGTH));
+		double yaw;
+		//This takes the width of the image in pixels and divides it by the horizontal angle,
+		//giving the number of degrees per pixel
+		double deg_per_pixel= LOGITECH_RES_WIDTH / LOGITECH_HORIZ_ANGLE;
+		double center_col = (LOGITECH_RES_WIDTH - 1) / 2; //Center column of image, -1 is to account for 0 indexing
+
+		int contour_x = upper_tape.x;//column of center of contour (tape)
+		yaw = (contour_x - center_col) * deg_per_pixel;
+		//(contour_x - center_col) is the horizontal distance between 
+		//the center of the image and the center of the target in pixels, so 
+		//you multiply that by the degrees/pixel conversion factor to get degrees
+
+		return yaw;
 	}
 
 	public double getDistance(Rect upper_tape) {
-		//double apparent_width=upper_tape.width;
-		//double horiz_distance= (UPPER_TAPE_WIDTH * LOGITECH_FOCAL_LENGTH) / apparent_width;
-		//return (UPPER_TAPE_WIDTH * LOGITECH_FOCAL_LENGTH) / upper_tape.width;
-		return TOWER_HEIGHT/math.sin(LOGITECH_HORIZ_ANGLE);
+		double apparent_width=upper_tape.width;
+		double horiz_distance= (UPPER_TAPE_WIDTH * LOGITECH_FOCAL_LENGTH) / apparent_width;
+		return horiz_distance;
+
 	}
 	public double getAngle(double horiz_dist) {
 		return (Math.toDegrees(Math.atan(TOWER_HEIGHT/horiz_dist)));
